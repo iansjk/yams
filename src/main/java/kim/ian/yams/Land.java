11ead62e6fba8cffaceed0c7999a61231132b360
Ian Kim <iansjk@gmail.com>
2014-07-21 03:54:31 -0400

    Forgot all about lands!

--- src/main/java/kim/ian/yams/Land.java
+++ src/main/java/kim/ian/yams/Land.java
@@ -1,4 +1,11 @@
 package kim.ian.yams;
 
+import kim.ian.yams.enums.LandSubtype;
+
+import java.util.Set;
+
 public interface Land {
+    public boolean isBasicLand();
+    public Set<LandSubtype> getSubtypes();
+    public ManaCost tap();
 }
---
+++ src/main/java/kim/ian/yams/enums/LandSubtype.java
@@ -0,0 +1,9 @@
+package kim.ian.yams.enums;
+
+public enum LandSubtype {
+    Plains,
+    Island,
+    Swamp,
+    Mountain,
+    Forest
+}
